c9ec8ad70f3bfa17166c08edcab14da8b68ffc62
AmNotAGoose <alexyihengwu@gmail.com>
2024-08-25 22:18:14 -0400

    added sample map with attribution and simple window

---
+++ README.md
@@ -0,0 +1,4 @@
+# simple-vsrg
+A simple osu!mania compatible vsrg.
+
+Test map from: https://osu.ppy.sh/beatmapsets/2157704#mania/4595519
---
+++ main.py
@@ -0,0 +1,18 @@
+import pygame
+import sys
+
+pygame.init()
+
+screen = pygame.display.set_mode((1000, 1000))
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    screen.fill((0, 0, 0))
+    pygame.display.flip()
+
+pygame.quit()
+sys.exit()
